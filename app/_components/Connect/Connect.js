e794f1e17fc3b450eeabfc3ddfb00c9c79812732
Эмин Байрамов <eminbayramov1991@gmail.com>
2025-10-10 14:09:33 +0400

    user name update 1.03

--- app/_components/Connect/Connect.js
+++ app/_components/Connect/Connect.js
@@ -3,46 +3,20 @@ import styles from "./connect.module.css";
 import { usePrivy } from "@privy-io/react-auth";
 import Image from "next/image";
 import connectedImg from "@/public/SLEEPYSIGN.webp";
-import { useEffect, useState } from "react";
 
 export default function Connect() {
   const { authenticated, user, login, logout, ready } = usePrivy();
 
-  const [profileName, setProfileName] = useState("");
-
-  useEffect(() => {
-    async function fetchUP() {
-      if (user?.wallet?.address) {
-        try {
-          const res = await fetch(
-            `https://api.universalprofile.cloud/v1/profiles/${user.wallet.address}`
-          );
-          const data = await res.json();
-          // Check if the name exists
-          setProfileName(data?.name || data?.description || "");
-          console.log("UP Data:", data);
-        } catch (err) {
-          console.error("Error fetching UP:", err);
-        }
-      }
-    }
-    fetchUP();
-  }, [user]);
-
   if (!ready) return null;
 
   if (authenticated) {
-    const userName =
-      profileName ||
-      user?.wallet?.address?.slice(0, 6) ||
-      "User";
-
-    console.log(profileName);
-    console.log(user?.wallet?.address);
+    const userName = user?.name || user?.wallet?.address?.slice(0, 6) || "User";
 
     return (
       <div className={styles.connect}>
-        <button onClick={logout}>{userName}</button>
+        <button onClick={logout}>
+          {userName}
+        </button>
       </div>
     );
   }
