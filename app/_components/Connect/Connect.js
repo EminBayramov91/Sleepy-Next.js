57eb91b79b15853db782dd98e7c4f4920639bc37
Эмин Байрамов <eminbayramov1991@gmail.com>
2025-10-23 17:57:35 +0400

    new commit 1.0.2

--- app/_components/Connect/Connect.js
+++ app/_components/Connect/Connect.js
@@ -1,17 +1,18 @@
 "use client";
 import styles from "./connect.module.css";
 import { usePrivy } from "@privy-io/react-auth";
-import Image from "next/image";
-import connectedImg from "@/public/SLEEPYSIGN.webp";
+import UserAvatar from "@/app/_components/UserAvatar/UserAvatar";
 
 export default function Connect() {
   const { authenticated, user, login, logout, ready } = usePrivy();
+  console.log(authenticated);
+  console.log(user);
 
   if (authenticated) {
     return (
-      <div className={styles.connect}>
-        <button onClick={logout}>Connected</button>
-      </div>
+      <>
+        <UserAvatar />
+      </>
     );
   }
 
